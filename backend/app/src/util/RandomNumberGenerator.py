f8d875b255fdef6df29b78ab7ed84f465c6737be
Zach McFadden <zrmmaster92@gmail.com>
2026-02-11 16:30:23 -0800

    Backend unit testing framework (#3)
    Co-authored-by: Zach McFadden <zachmcfadden@Zachs-MacBook-Air.local>

--- backend/app/src/util/RandomNumberGenerator.py
+++ backend/app/src/util/RandomNumberGenerator.py
@@ -12,14 +12,22 @@ class RandomNumberGenerator:
   def __init__(self):
     pass
 
+  """
+    Generates a random string (combination of ASCII chars and numbers) of a length specified by the function caller.
+    
+    Usage Example:
+      randomString =  RandomNumberGenerator.generate_random_string(5)
+      print(len(randomString)) # Should be a random string of length 5
+      print(randomString) # Whatever the random string was 
+  """
   @staticmethod
   def generate_random_string(length: int) -> str:
     r_string_builder: str = ''
-    rand_int: int = 0
     for i in range(length):
       rand_int = random.randint(0,1000000)
 
       if rand_int % 2 == 0:
+        # TODO - improve - not just ASCII uppercase?
         r_string_builder += random.choice(string.ascii_uppercase)
       else:
         r_string_builder += str(random.randint(0,9))
