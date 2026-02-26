4619b3625ac7aa8d45eaf210cfda173c6bd1504d
Kay M <bomo794@gmail.com>
2026-02-25 19:29:04 -0800

    Revert "added setup function"
    This reverts commit 02cbfbca60ebc11c4c35f9c243a80d25c4ab50c8.
    
    :wq
    :wq
    :wq

--- backend/app/src/Setup.py
+++ backend/app/src/Setup.py
@@ -3,13 +3,8 @@
   Date: 2/1/26
   Synopsis: Container class for executing general startup setup steps
 """
-from backend.app.src.util.LogFactory import LogFactory
-# from .Configuration import CONF_INSTANCE
+from src.util.LogFactory import LogFactory
+from src.Configuration import CONF_INSTANCE
 
 class Setup:
-    def __init__(self):
-        pass
-
-def setup():
-    LogFactory.main_log()
-    LogFactory.MAIN_LOG.info(f"Executing Application Setup!")
+    pass
