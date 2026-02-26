0376beeaf13241a08b47bfa0eefaed2ef069433f
Kay M <bomo794@gmail.com>
2026-02-25 19:19:07 -0800

    added setup function

--- backend/app/src/Setup.py
+++ backend/app/src/Setup.py
@@ -3,8 +3,13 @@
   Date: 2/1/26
   Synopsis: Container class for executing general startup setup steps
 """
-from src.util.LogFactory import LogFactory
-from src.Configuration import CONF_INSTANCE
+from backend.app.src.util.LogFactory import LogFactory
+# from .Configuration import CONF_INSTANCE
 
 class Setup:
-    pass
+    def __init__(self):
+        pass
+
+def setup():
+    LogFactory.main_log()
+    LogFactory.MAIN_LOG.info(f"Executing Application Setup!")
