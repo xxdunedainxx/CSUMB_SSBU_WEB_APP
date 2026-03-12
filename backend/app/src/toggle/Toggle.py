1d53f90da0be576b41f1b42d929014b9436a3bd6
KayMakk <bomo794@gmail.com>
2026-03-11 20:20:31 -0700

    added more comments, and set up singleton instance

--- backend/app/src/toggle/Toggle.py
+++ backend/app/src/toggle/Toggle.py
@@ -12,20 +12,23 @@ class ToggleService:
 
     EXAMPLE_TOGGLE="EXAMPLE_TOGGLE"
 
-    """
-        Class constructor
-    """
-    def __new__(cls): ##
+   # controls object creation before __init__ and helps create Singleton Instance
+    def __new__(cls, y):
         if cls.INSTANCE is None:
             cls.INSTANCE = super().__new__(cls) # creates new instance of the class
         return cls.INSTANCE
 
 
+    # Class constructor
     def __init__(self, toggleConfigurationFilePath: str):
+        # checks if _initialized exists and if its true otherwise set up the instance
+        if hasattr(self, "_initialized") and self._initialized:
+            return
         # Toggles are a dictionary of strings --> bools
         self.__TOGGLES: dict[str, bool] = {}
         # when an object is created, convert the file path to a dictionary and store in Toggles
         self.__load_toggle_configuration(toggleConfigurationFilePath)
+        _initialized = True
 
     def __load_toggle_configuration(self, toggleConfPath):
         # with is a context manager that closes the file given after we're done
