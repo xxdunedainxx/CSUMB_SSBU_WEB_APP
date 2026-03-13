e97fb1af4c322c36a4122e2b0c7ef7a0f4ce548f
KayMakk <bomo794@gmail.com>
2026-03-12 20:40:00 -0700

    Added unit tests for feature toggle

--- backend/app/src/toggle/Toggle.py
+++ backend/app/src/toggle/Toggle.py
@@ -37,6 +37,6 @@ class ToggleService:
             self.__TOGGLES = json.load(tempFile)
 
     def is_toggle_enabled(self, toggleName: str) -> bool:
-        if not toggleName in self.__TOGGLES:
+        if toggleName not in self.__TOGGLES:
             raise KeyError(f"{toggleName} does not exist")
         return self.__TOGGLES[toggleName]
--- backend/app/test/unit/util/ToggleTests/test_toggles.py
+++ backend/app/test/unit/util/ToggleTests/test_toggles.py
@@ -1,10 +1,25 @@
 import unittest
+from src.toggle.Toggle import ToggleService
+from src.util.LogFactory import LogFactory
+from test.util.decorators.Toggle import enabled
 
+from contextlib import nullcontext as does_not_raise
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+@enabled
+def test_toggles():
+    LogFactory.MAIN_LOG.info(f"RUNNING feature toggle tests")
+    unittest.main(module=__name__, exit=False)
 
+class TestFeatureToggles(unittest.TestCase):
+
+    def test_check_error(self):
+        with self.assertRaises(KeyError) as tc:
+            test_one.is_toggle_enabled("RANDOM_TOGGLE")
+
+    def test_check_true(self):
+        assert test_one.is_toggle_enabled("NEW_FEATURE") ==  True
 
 if __name__ == '__main__':
     unittest.main()
+
+test_one = ToggleService("test/unit/util/ToggleTests/Example.json")
