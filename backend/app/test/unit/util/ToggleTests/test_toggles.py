2da4bedc3ec39436bd6a4b389ee32e63f394d07f
Zach McFadden <zachmcfadden@Zachs-MacBook-Air.local>
2026-03-13 16:59:54 -0700

    fix up pathing stuff and enable tests

--- backend/app/test/unit/util/ToggleTests/test_toggles.py
+++ backend/app/test/unit/util/ToggleTests/test_toggles.py
@@ -2,7 +2,7 @@ import unittest
 from src.toggle.Toggle import ToggleService
 from src.util.LogFactory import LogFactory
 from test.util.decorators.Toggle import enabled
-
+import os
 from contextlib import nullcontext as does_not_raise
 
 @enabled
@@ -13,11 +13,13 @@ def test_toggles():
 class TestFeatureToggles(unittest.TestCase):
 
     # test that an error raises when a key doesn't exist
+    @enabled
     def test_check_error(self):
         with self.assertRaises(KeyError) as tc:
             test_one.is_toggle_enabled("RANDOM_TOGGLE")
 
     # tests that no error raises when key does exist
+    @enabled
     def test_check_true(self):
         with does_not_raise():
             assert test_one.is_toggle_enabled("NEW_FEATURE")
@@ -25,4 +27,5 @@ class TestFeatureToggles(unittest.TestCase):
 if __name__ == '__main__':
     unittest.main()
 
-test_one = ToggleService("util/ToggleTests/Example.json")
+base_dir = os.path.dirname(os.path.abspath(__file__))
+test_one = ToggleService(f"{base_dir}/Example.json")
