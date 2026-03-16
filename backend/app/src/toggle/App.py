2012f00367aa2499aebaa6763cdcf9f7f066b019
Kay M <bomo794@gmail.com>
2026-03-16 10:49:25 -0700

    fixed return statement with specific toggle

--- backend/app/src/toggle/App.py
+++ backend/app/src/toggle/App.py
@@ -17,7 +17,7 @@ def index(toggle_name: str):
     test = {
         toggle_name: test_one.is_toggle_enabled(toggle_name)
     }
-    return jsonify({test})
+    return jsonify(test)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5555)
