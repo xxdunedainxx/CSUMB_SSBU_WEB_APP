dfc82f685a337e0f6c9d771c6675b931cd0894fb
Olukayode Makinde-Odusola <108780040+KayyMak@users.noreply.github.com>
2026-03-16 09:36:33 -0700

    Toggle Class Setup (#12)

--- backend/app/test/unit/UnitTests.py
+++ backend/app/test/unit/UnitTests.py
@@ -7,6 +7,7 @@ from src.util.LogFactory import LogFactory
 from test.unit.util.RandomNumnberGeneratorUnitTesting import random_number_generation_tests
 from test.unit.util.FileIOUnitTests import file_io_unit_tests
 from test.unit.util.test_email import test_emails
+from test.unit.util.ToggleTests.test_toggles import test_togglers
 
 class UnitTests:
 
@@ -16,4 +17,5 @@ class UnitTests:
     random_number_generation_tests()
     file_io_unit_tests()
     test_emails()
+    test_togglers()
 
