f2b38361354ab5d7d25cfd119b348af8c3881269
Zach McFadden <zachmcfadden@Zachs-MacBook-Air.local>
2026-03-21 18:12:18 -0700

    Docker backup script starteR

---
+++ inf/db/scripts/backup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#########
+# Author: TODO
+# Date: TODO
+# Synopsis: Postgres DB Backup script TODO
+# Example usage: TODO
+#########
+
+echo "Running backup..."
+
+# TODO Use PG Dump to grab database and push to a file under /tmp
+
+# TODO Make sure to PRUNE old db dumps (older than 'x' days)
