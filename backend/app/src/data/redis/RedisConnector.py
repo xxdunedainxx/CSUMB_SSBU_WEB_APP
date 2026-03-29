92176c1cd153ca40f5dfe6a69c8c6ae0228bf7f7
Micah <micahheneveld@gmail.com>
2026-03-29 08:56:55 -0700

    Added push/pop redis functionality

--- backend/app/src/data/redis/RedisConnector.py
+++ backend/app/src/data/redis/RedisConnector.py
@@ -14,6 +14,18 @@ class RedisConnector:
 
     def delete_value(self, key):
         return self.client.delete(key)
+    
+    def enqueue(self, queue_name, value):
+        return self.client.lpush(queue_name, value)
+    
+    def dequeue(self, queue_name):
+        return self.client.rpop(queue_name)
+    
+    def queue_length(self, queue_name):
+        return self.client.llen(queue_name)
+    
+    def dequeu_blocking(self, queue_name, timeout=0):
+        return self.client.brpop(queue_name, timeout=timeout)
 
     def close(self):
         # Close the connection
