ecc7a44d5c5d7fa35719ad3eb0eb6473d63a87b6
Zach McFadden <zrmmaster92@gmail.com>
2026-04-29 16:36:55 -0700

    Crypto Service (Encryption + Hashing) (#16)

--- backend/app/src/sec/Crypto.py
+++ backend/app/src/sec/Crypto.py
@@ -1,1 +1,90 @@
-# Crypto service
+"""
+    Author: Zach Mcfadden
+    Date: 4.6.2026
+    Synopsis: General Container class for any cryptographic related functions such as hashing and encryption.
+"""
+import base64
+
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+import hashlib
+
+from src.util.FileIO import FileIO
+
+class CryptoService:
+    ENCODING="utf-8"
+
+    """
+        Util methods for hashing below 
+    """
+    @staticmethod
+    def sha256_hash_string(data: str) -> str:
+        return hashlib.sha256(data.encode(CryptoService.ENCODING)).hexdigest()
+
+    @staticmethod
+    def sha256_hash_bytes(data: str) -> bytes:
+        return hashlib.sha256(data.encode(CryptoService.ENCODING)).digest()
+
+    def __init__(self, initiazationVectorFile: str, keyFile: str):
+        self.__establish_aes_cipher(initiazationVectorFile, keyFile)
+
+    """
+        Encrypts a series of bytes, and returns the cyphertext as bytes 
+    """
+    def encrypt_bytes(self, data: bytes) -> bytes:
+        return self.aes256.encrypt(
+            self.iv,
+            data,
+            associated_data=None
+        )
+
+    """
+        Decrypts a series of bytes, and returns the plain text as bytes 
+    """
+    def decrypt_to_bytes(self, data: bytes) -> bytes:
+        return self.aes256.decrypt(
+            self.iv,
+            data,
+            associated_data=None
+        )
+
+    """
+        Encrypts a string object and will decode the ciphertext bytes into usable string 
+    """
+    def encrypt_string(self, data: str) -> str:
+        return base64.b64encode(self.aes256.encrypt(
+            self.iv,
+            data.encode(CryptoService.ENCODING),
+            associated_data=None
+        )).decode(CryptoService.ENCODING)
+
+    """
+        Decrypts the ciphertext data string to a plaintext string 
+    """
+    def decrypt_to_string(self, data: str) -> str:
+        return self.aes256.decrypt(
+            self.iv,
+            base64.b64decode(data.encode(CryptoService.ENCODING)),
+            associated_data=None
+        ).decode(CryptoService.ENCODING)
+
+    """
+        Establishes the AES cipher 
+    """
+    def __establish_aes_cipher(self, ivPath: str, keyPath: str):
+        aes256 = AESGCM(
+            self.__read_and_delete_file(keyPath)
+        )
+
+        self.aes256=aes256
+        self.iv=self.__read_and_delete_file(ivPath)
+
+    """ 
+        Reads a file and deletes it 
+    """
+    def __read_and_delete_file(self, path: str) -> bytes:
+        b=FileIO.read_bytes_from_file(path)
+
+        FileIO.delete_file(path)
+
+        return b
+
---
+++ backend/app/test/unit/sec/CryptoServiceUnitTests.py
@@ -0,0 +1,60 @@
+"""
+    Author: Ishaya Iliya
+    Date: 2.18.2026
+    Synopsis: Class to detect and scrub PII from strings
+"""
+import os
+
+from src.sec.Crypto import CryptoService
+from src.util.FileIO import FileIO
+from src.util.LogFactory import LogFactory
+
+from test.util.decorators.Toggle import enabled
+
+import unittest
+
+@enabled
+def crypto_service_unit_testing():
+    LogFactory.MAIN_LOG.info(f"RUNNING Crypto Service tests")
+    unittest.main(module=__name__, exit=False)
+
+class CryptoServiceUnitTests(unittest.TestCase):
+
+    @enabled
+    def test_hash_functions(self):
+        assert(CryptoService.sha256_hash_string("test") != CryptoService.sha256_hash_string("test2"))
+        assert(CryptoService.sha256_hash_string("test") == CryptoService.sha256_hash_string("test"))
+        assert(CryptoService.sha256_hash_string("test") == "9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08")
+
+    @enabled
+    def test_aes_encryption(self):
+        ivFile="tmpIV"
+        keyFile="tmpAesKey"
+        FileIO.generate_byte_file(
+            keyFile,
+            os.urandom(32)
+        )
+
+        FileIO.generate_byte_file(
+            ivFile,
+            os.urandom(16)
+        )
+
+        cryptoService=CryptoService(
+            ivFile,
+            keyFile
+        )
+
+        bytesToEncrypt=os.urandom(10)
+        encryptedData=cryptoService.encrypt_bytes(bytesToEncrypt)
+        assert(cryptoService.decrypt_to_bytes(encryptedData) == bytesToEncrypt)
+
+        stringToEncrypt="test"
+        encryptedData=cryptoService.encrypt_string(stringToEncrypt)
+        assert(cryptoService.decrypt_to_string(encryptedData) == stringToEncrypt)
+
+        # Ensure the tmp files were removed
+        assert(FileIO.file_exists(ivFile) == False and FileIO.file_exists(keyFile) == False)
+
+if __name__ == "__main__":
+    unittest.main()
